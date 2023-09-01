46bb8841687d405e1846513c87ed41b84d246c3b
TaiveyonShaw <shawt6@wwu.edu>
2023-09-01 04:50:01 -0700

    test

--- components/main.js
+++ components/main.js
@@ -27,10 +27,10 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-const blurHeader = () => {
-  const header = document.getElementsById('header')
-  // When the scroll is greater than 50 viewport Height, add the blur-header class
-  this.scrollY >= 50 ? header.classList.add('blur-header')
+const blurHeader = () =>{
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add('blur-header') 
                      : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
